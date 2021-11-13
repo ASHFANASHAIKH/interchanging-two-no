304e335a4eee24a5b675b65fd49cd5ce8f5b1b00
ashfana <ashfana@DESKTOP-NPR6MTA>
2021-11-13 12:50:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interchanging_two_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7, b = 3, temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("Values after swapping are:");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
